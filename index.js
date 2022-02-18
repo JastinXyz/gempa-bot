ef36e94897d3cb593921e458bb6f2f2151be65ff
JstnLT <jastinlinggar@gmail.com>
2022-02-18 12:04:05 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+const aoijs = require("aoi.js");
+const axios = require('axios');
+
+const bot = new aoijs.Bot({
+  token: process.env.TOKEN,
+  prefix: "dung",
+  intents: "all",
+});
+
+// const voice = new aoijs.Voice(bot, {
+//   cache: {
+//     cacheType: "Memory",//Disk | None
+//     enabled: true,
+//   },
+// }); 
+
+bot.onMessage();
+
+bot.status({
+  text: "wong turu",
+  type: "WATCHING",
+  time: 12
+})
+
+bot.command({
+  name: "ping",
+  code: `My Ping Is \`$ping ms\` `,
+});
+
+bot.loopCommand({
+  code: `
+  $djsEval[
+  (async() => {
+
+    const Discord = require('discord.js')
+    const jsonfile = require('jsonfile')
+    jsonfile.readFile('./data.json', function (err, obj) {
+
+if(obj.date !== "$getObjectProperty[Infogempa.gempa.DateTime]") {
+  const exampleEmbed = new Discord.MessageEmbed()
+    .setColor(Math.floor(Math.random() * 16777215))
+    .setTitle(':warning: Telah Terjadi Gempa Bumi :warning: ')
+    .addFields(
+        { name: 'Waktu Gempa', value: '$getObjectProperty[Infogempa.gempa.Tanggal], $getObjectProperty[Infogempa.gempa.Jam]', inline: true },
+        { name: 'Magnitudo', value: '$getObjectProperty[Infogempa.gempa.Magnitude]', inline: true },
+        { name: 'Kedalaman', value: '$getObjectProperty[Infogempa.gempa.Kedalaman]', inline: true },
+        { name: 'Koordinat', value: '$getObjectProperty[Infogempa.gempa.Coordinates]', inline: true },
+        { name: 'Lintang', value: '$getObjectProperty[Infogempa.gempa.Lintang]', inline: true },
+        { name: 'Bujur', value: '$getObjectProperty[Infogempa.gempa.Bujur]', inline: true },
+        { name: 'Wilayah', value: '$getObjectProperty[Infogempa.gempa.Wilayah] ($getObjectProperty[Infogempa.gempa.Dirasakan])', inline: true },
+        { name: 'Potensi', value: '$getObjectProperty[Infogempa.gempa.Potensi]', inline: true },
+    )
+    .setImage('https://data.bmkg.go.id/DataMKG/TEWS/$getObjectProperty[Infogempa.gempa.Shakemap]')
+    .setTimestamp()
+    .setFooter({ text: 'Powered by BMKG' });
+
+channel.send({ content: '@everyone', embeds: [exampleEmbed] }).then(() => {
+
+  const write = {
+    date: "$getObjectProperty[Infogempa.gempa.DateTime]"
+  }
+
+ jsonfile.writeFile('./data.json', write, function (err) {
+   if (err) console.error(err)
+   console.log('updated')
+ })
+
+})
+  } else {
+
+  }
+
+})
+  $createObject[$jsonRequest[https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json;]]
+  })()
+  ]
+  `,
+  channel: process.env.CHANNEL_ID,
+  executeOnStartup: true,
+  every: 3000,
+});
+
+bot.command({
+  name: "e",
+  code: `$eval[$message]
+$onlyForIDs[$botOwnerID;:eyes:]`,
+});
